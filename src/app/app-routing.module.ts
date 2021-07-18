150a2865fcc135b25c1694e7b65b04b29833b237
rangaswamy-c <rangaswamy.c@sonata-software.com>
2021-07-18 22:15:17 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { AuthGuard } from './helpers';
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'account', loadChildren: accountModule },
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/helpers/fake-backend.ts
@@ -0,0 +1,72 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, materialize, dematerialize } from 'rxjs/operators';
+
+const usersKey = 'use-case1-users';
+let users = JSON.parse(localStorage.getItem(usersKey)) || [];
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+
+        return handleRoute();
+
+        function handleRoute() {
+            switch (true) {
+                case url.endsWith('/users/authenticate') && method === 'POST':
+                    return authenticate();
+                case url.endsWith('/users/register') && method === 'POST':
+                    return register();
+                default:
+                    return next.handle(request);
+            }    
+        }
+
+        function authenticate() {
+            const { username, password } = body;
+            const user = users.find(x => x.username === username && x.password === password);
+            if (!user) return error('Username or password is invalid');
+            return ok({
+                ...basicDetails(user),
+                token: 'fake-jwt-token'
+            })
+        }
+
+        function register() {
+            const user = body
+
+            if (users.find(x => x.username === user.username)) {
+                return error('Username "' + user.username + '" is already taken')
+            }
+
+            user.id = users.length ? Math.max(...users.map(x => x.id)) + 1 : 1;
+            users.push(user);
+            localStorage.setItem(usersKey, JSON.stringify(users));
+            return ok();
+        }
+
+
+        function ok(body?) {
+            return of(new HttpResponse({ status: 200, body }))
+                .pipe(delay(500)); 
+        }
+
+        function error(message) {
+            return throwError({ error: { message } })
+                .pipe(materialize(), delay(500), dematerialize()); 
+        }
+
+        function basicDetails(user) {
+            const { id, username, firstName, lastName } = user;
+            return { id, username, firstName, lastName };
+        }
+    }
+}
+
+export const fakeBackendProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
